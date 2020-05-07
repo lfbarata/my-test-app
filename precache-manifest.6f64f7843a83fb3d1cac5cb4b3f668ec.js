9a56529d7bca39799260a8c6f77f3c745a36df00
lbarata <luis_barata@intuit.com>
2020-05-06 22:34:53 -0600

    Updates

---
+++ precache-manifest.6f64f7843a83fb3d1cac5cb4b3f668ec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbc971ee5bc9c0948f987b1868c3bdd7",
+    "url": "/my-test-app/index.html"
+  },
+  {
+    "revision": "e79af42935f5021992d5",
+    "url": "/my-test-app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "25576bf9087319909653",
+    "url": "/my-test-app/static/js/2.6b7123c4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-test-app/static/js/2.6b7123c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e79af42935f5021992d5",
+    "url": "/my-test-app/static/js/main.e4b08898.chunk.js"
+  },
+  {
+    "revision": "a495ba79e5f4b85574b3",
+    "url": "/my-test-app/static/js/runtime-main.3e201605.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-test-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
